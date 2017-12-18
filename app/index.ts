4bfe8c884d3719a2830b69dd8ea9587269c1807b
christhopher.leao <christhopherleao@icloud.com>
2017-12-18 14:52:04 -0200

    Basic Compilation Config

---
+++ app/index.ts
@@ -0,0 +1,11 @@
+import moment from 'moment';
+// import { Message } from './model';
+
+import './main.scss';
+
+// const template: any = require('./messages.html');
+// const logo: any = require('./images/especializa_logo.jpg');
+
+if (module && module.hot) {
+    module.hot.accept();
+}
---
+++ webpack.config.js
@@ -0,0 +1,138 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+
+const extractSass = new ExtractTextWebpackPlugin({
+    filename: '[name].[contenthash:8].bundle.css',
+    disable: false,
+});
+
+const minify = {
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    removeComments: true,
+};
+const config = {
+    entry: {
+        main: './app/index.ts',
+        // oldMessages: './app/old-messages.ts',
+        vendor: ['whatwg-fetch'],
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[hash:8].bundle.js',
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'commons',
+            filename: 'commons.[hash:8].bundle.js',
+            minChunks: 2,
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'app', 'index.html'),
+            filename: 'index.html',
+            chunks: ['main', 'commons', 'vendor'],
+            minify,
+        }),
+        // new HtmlWebpackPlugin({
+        //     template: path.join(__dirname, 'app', 'old-messages.html'),
+        //     filename: 'old-messages.html',
+        //     chunks: ['oldMessages', 'commons', 'vendor'],
+        //     minify,
+        // }),
+        extractSass,
+        new UglifyJsWebpackPlugin({
+            sourceMap: true,
+        }),
+        new CompressionWebpackPlugin({
+            asset: '[path].gz',
+        }),
+    ],
+    module: {
+        rules: [{
+                test: /\.html$/,
+                loader: 'html-es6-template-loader',
+                exclude(filePath) {
+                    return filePath === path.join(__dirname, 'app', 'index.html');
+                },
+                query: {
+                    transpile: true,
+                },
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        ['es2015', {
+                            modules: false,
+                        }],
+                    ],
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.ts$/,
+                loader: 'awesome-typescript-loader',
+            },
+            {
+                test: /\.s[ac]ss$/,
+                loader: extractSass.extract({
+                    use: [{
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader'
+                        },
+                        {
+                            loader: 'sass-loader'
+                        },
+                    ],
+                    fallback: 'style-loader',
+                }),
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)/,
+                use: [{
+                        loader: 'url-loader',
+                        query: {
+                            limit: 5000,
+                            name: '[name].[hash:8].[ext]',
+                        },
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        query: {
+                            mozjpeg: {
+                                quality: 65,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
+
+if (process.env.NODE_ENV === 'development') {
+    config.watch = true;
+    config.devtool = 'source-map';
+} else if (process.env.NODE_ENV === 'hot') {
+    config.devtool = 'source-map';
+    config.devServer = {
+        hot: true,
+    };
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+}
+
+module.exports = config;
