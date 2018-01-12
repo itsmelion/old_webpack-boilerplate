14de451a965ebe3556eac43acc7dc6ce53297216
christhopher.leao <christhopherleao@icloud.com>
2018-01-12 19:40:32 -0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,15 @@
+if (module && module.hot) {
+    module.hot.accept();
+}
+
 import moment from 'moment';
 // import { Message } from './model';
 
 import './main.scss';
 
-const main: HTMLMainElement = document.createElement('main');
-const button: HTMLButtonElement = document.createElement('button');
-main.innerHTML =    `<h2>Right now is:<br>
-                    <b>${moment().format('DD/MM/YYYY hh:mm:ss')}</b></h2>`;
-button.innerHTML = 'Lazy load something..';
-main.appendChild(button);
-
-// {Tried to async.. not very functional}
-button.onclick = e => require(/* webpackChunkName: "print"*/ './print')
-.then((module) => module() );
+window.document.getElementsByTagName('body')[0]
+    .innerHTML = '<h2>Right now is: <b>' +
+    moment().format('DD/MM/YYYY hh:mm:ss') + '</b></h2>';
 
 // const template: any = require('./messages.html');
 // const logo: any = require('./images/especializa_logo.jpg');
-
-if (module && module.hot) {
-    module.hot.accept();
-}
